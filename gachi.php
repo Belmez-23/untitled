4e76aa6528bc9dd14753ccce0a10526e16dafda9
Username <afoniabrando@mail.ru>
2021-07-01 16:53:33 +0300

    почему гачи делает лишнюю строку в 2 элемента?

--- gachi.php
+++ gachi.php
@@ -3,7 +3,23 @@ $ishod = file('data.txt');
 //$ishod = $_POST;
 $s = explode(' ', $ishod[rand(0, 8)]);
 $ss = count($s);
-$gachi = array('300 bucks', 'club', 'buddy', 'Ass', 'boy', 'Fuck', 'Fuck you', 'leather man', 'door', 'Deep dark fantasies', 'Dungeon master', 'Cumming', 'slave', 'Stick your finger', 'amazing');
+$gachi = array(
+    '300 bucks',
+    'club',
+    'buddy',
+    'Ass',
+    'boy',
+    'Fuck',
+    'Fuck you',
+    'leather man',
+    'door',
+    'Deep dark fantasies',
+    'Dungeon master',
+    'Cumming',
+    'slave',
+    'Stick your finger',
+    'amazing'
+);
 $gg = count($gachi);
 foreach ($gachi as $g) {
     $rs = rand(0, $ss);
