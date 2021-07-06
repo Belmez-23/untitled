ec907e85aed6bd8cd24b73696ce6b3f5157f4206
Username <afoniabrando@mail.ru>
2021-07-06 11:08:55 +0300

    Оптимизировано зеркало

--- getMirrorMatrix.php
+++ getMirrorMatrix.php
@@ -1,26 +1,27 @@
 <?php
+
 function getMirrorMatrix($arrays){
 
     $mirror = [];
-    for($a = 0; $a < count($arrays); $a++) {
-        //возможно более лаконичное решение с циклом захватывающим только индексы левой стороны матрицы?
-        $array = $arrays[$a];
-        $left = 0;
-        $right = count($array) - 1;
-        $center = count($array) / 2;
+    $center = count($arrays[0]);
 
-        while ($left < $center) {
-            $array[$right--] = $array[$left++];
+    foreach ($arrays as $array) {
+
+        for ($left = 0; $left < $center; $left++) {
+            $right = count($array)-$left-1;
+            $array[$right] = $array[$left];
         }
         array_push($mirror, $array);
     }
-
     return $mirror;
+
 }
 
 var_dump(getMirrorMatrix([
-    [11, 12, 13, 14],
-    [21, 22, 23, 24],
-    [31, 32, 33, 34],
-    [41, 42, 43, 44],
+    [11, 12, 13, 14, 15, 16],
+    [21, 22, 23, 24, 25, 26],
+    [31, 32, 33, 34, 35, 36],
+    [41, 42, 43, 44, 45, 46],
+    [51, 52, 53, 54, 55, 56],
+    [61, 62, 63, 64, 65, 66],
 ]));
